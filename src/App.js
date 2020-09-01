cb8612ddef5fe7ffbca22bce615d26cfb6aa12eb
Your Name <olucasellery@gmail.com>
2020-09-01 17:12:40 -0300

    Input retirado (podendo ser recolocado no futuro)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
-import Input from "./Components/Input";
 import Catalogo from "./Components/Catalogo";
 import { Component } from "react";
-import Quadrinho from "./dados/Quadrinho";
 // import Catalogo from "./Components/Catalogo";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.quadrinhos = new Quadrinho();
-  }
-
   render() {
     return (
       <div className="App">
         <Header />
-
-        <Input />
 
         <Catalogo />
       </div>
